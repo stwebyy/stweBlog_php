0859ba40017ba6ba76025ef12d7185dcab84dd13
stwebyy <stweb.yy@gmail.com>
2021-03-15 00:22:50 +0900

    reset.cssとstyle.cssの作成・ヘッダーの作成

---
+++ backend/resources/views/commons/header.blade.php
@@ -0,0 +1,15 @@
+<header class="blog-header py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center mb-3">
+      <div class="offset-4 col-4 text-center">
+        <a class="blog-header-logo text-dark" href="#">stweblog</a>
+      </div>
+    </div>
+    <ul class="nav-list">
+        <li class="nav-list-item">
+            <a href="#" class="nav-list-link">Home</a>
+        </li>
+        <li class="nav-list-item"><a href="#" class="nav-list-link">About</a></li>
+        <li class="nav-list-item"><a href="#" class="nav-list-link">ブログ/備忘録</a></li>
+        <li class="nav-list-item"><a href="#" class="nav-list-link">作り物たち</a></li>
+    </ul>
+</header>
--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -10,10 +10,14 @@
         <meta property="og:site_name" content="stweblog" />
         <meta property="og:locale" content="ja_JP" />
         <!-- Bootstrap CSS -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
             integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('assets/css/Reboot.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
         @section('css')
         @show
