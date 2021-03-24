dcd086722bca02b38dda6ee83eadcef9dc50fa46
stwebyy <stweb.yy@gmail.com>
2021-03-24 23:14:36 +0900

    bladeを各種ファイルに切り分け

---
+++ backend/resources/views/commons/aside.blade.php
@@ -0,0 +1,65 @@
+<aside class="col-4">
+    <div class="bg-white base__BoxShadow">
+        <div class="right__Box">
+            <h5 class="right__Header">Recent Posts</h5>
+            <div class="recent__Posts">
+                <a href="#">
+                    <p class="fs-12 color-light-gray"><i class="fas fa-calendar-week"></i><span class="ml-1">2021/03/17</span></p>
+                    <p class="recent__PostsTitle">テストタイトル</p>
+                </a>
+            </div>
+            <div class="recent__Posts">
+                <a href="#">
+                    <p class="fs-12 color-light-gray"><i class="fas fa-calendar-week"></i><span class="ml-1">2021/03/17</span></p>
+                    <p class="recent__PostsTitle">今日のSymfony FormTypeに関してまとめてみた</p>
+                </a>
+            </div>
+            <div class="recent__Posts">
+                <a href="#">
+                    <p class="fs-12 color-light-gray"><i class="fas fa-calendar-week"></i><span class="ml-1">2021/03/17</span></p>
+                    <p class="recent__PostsTitle">Laravel×React×Docker×AWS EC2のベストプラクティスに関してまとめた結果こうなった</p>
+                </a>
+            </div>
+        </div>
+        <div class="right__Box">
+            <h5 class="right__Header">Tags</h5>
+            <div class="tag__Items">
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;PHP</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Laravel</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Symfony</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Symfony</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Symfony</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Docker</a></object>
+                </div>
+                <div class="tag__ItemList">
+                    <object><a href="#?1"><i class="fas fa-tag"></i>&nbsp;Linux</a></object>
+                </div>
+            </div>
+        </div>
+        <div class="right__Box">
+            <h5 class="right__Header">Profile</h5>
+            <div class="text-center mt-3">
+                <img src="{{ asset('assets/images/profile_img.jpg') }}" width="50%" />
+            </div>
+            <div class="card-body">
+                <h5 class="card-title text-center"><a href="#">stweb</a></h5>
+                <p class="card-text text-center mb-2">PHP/Python<br>Laravel/Symfony/Django<br>FPS/鉄拳</p>
+                <div class="text-center">
+                    <a href="#" class="card-link"><img src="{{ asset('assets/images/GitHub-Mark-32px.png') }}" width="25px" /></a>
+                    <a href="#" class="card-link"><img src="{{ asset('assets/images/qiita_favicon.png') }}" width="25px" /></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</aside>
