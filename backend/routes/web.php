99953a670d15a1d6374dfaff7d591d363d6f800e
stwebyy <stweb.yy@gmail.com>
2021-04-04 12:34:08 +0900

    Voyagerインストールに伴い追加・更新されたファイル

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,23 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', 'TopController@index')->name('index');
 
 Route::prefix('blog')->group(function() {
     Route::get('/', 'BlogController@index')->name('blog_index');
     Route::get('/{id}', 'BlogController@show')->name('blog_show');
+});
+
+// Voyagerのデフォルトルーティング
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
