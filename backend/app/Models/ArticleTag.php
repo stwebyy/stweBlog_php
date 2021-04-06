b6abab16e43363af4095af7b2301dbed357740c4
stwebyy <stweb.yy@gmail.com>
2021-04-07 00:30:31 +0900

    Modelディレクトリ名の変更

---
+++ backend/app/Models/ArticleTag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleTag extends Model
+{
+    /**
+     * 関連table名
+     * 
+     * @var string
+     */
+    protected $table = 'article_tags';
+}
---
+++ backend/app/Models/Tag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    /**
+     * 関連table名
+     * 
+     * @var string
+     */
+    protected $table = 'tags';
+}
