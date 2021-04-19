e4b1fd6fb4c2d7235c497f86f282da4a268d7837
stwebyy <stweb.yy@gmail.com>
2021-04-20 01:31:24 +0900

    QiitaAPI用のサービスを実装

---
+++ backend/app/Service/QiitaApiService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Service;
+
+class QiitaApiService
+{
+    /**
+     * @var string
+     */
+    protected $qiita_token;
+
+    /**
+     * construct
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->qiita_token = env('QIITA_API_KEY');
+    }
+
+    /**
+     * cURLの設定、実行
+     * 
+     * @param string 対象URL
+     * @param string HTTPメソッド
+     * 
+     * @return json APIから取得した記事一覧
+     */
+    public function execCurlToQiitaApi($url, $method)
+    {
+        $ch = curl_init();
+        $option = [
+            CURLOPT_URL => env('QIITA_BASE_URL') . $url,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTPHEADER => [
+              'Authorization: Bearer ' . $this->qiita_token,
+              'Content-Type: application/json',
+            ],
+        ];
+        curl_setopt_array($ch, $option);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result);
+    }
+
+    /**
+     * Qiitaから自分の記事一覧を取得する
+     * 
+     * @return json
+     */
+    public function fetchQiitaArticleByApi()
+    {
+        $url = 'authenticated_user/items';
+
+        return $this->execCurlToQiitaApi($url, 'GET');
+    }
+}
