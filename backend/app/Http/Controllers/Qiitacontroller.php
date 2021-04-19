e6e6645a06c404662e9caa53561b9106d0cb6dac
stwebyy <stweb.yy@gmail.com>
2021-04-20 01:33:27 +0900

    動確用にアクションを設定

--- backend/app/Http/Controllers/Qiitacontroller.php
+++ backend/app/Http/Controllers/Qiitacontroller.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Qiita\QiitaRepositoryInterface;
+use App\Service\QiitaApiService;
 
 class Qiitacontroller extends Controller
 {
     /**
      * @return void
      */
-    public function __construct(QiitaRepositoryInterface $qiita)
+    public function __construct(
+        QiitaRepositoryInterface $qiita,
+        QiitaApiService $api
+    )
     {
         $this->qiita = $qiita;
+        $this->api = $api;
     }
 
     public function qiitaArticles()
     {
-        return $this->qiita->getQiitaArticles();
+        return $this->api->fetchQiitaArticleByApi();
     }
 }
