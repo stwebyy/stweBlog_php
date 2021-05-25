8fd2ae8c924ac97dccfedb47962ad9afb6ab0347
stwebyy <stweb.yy@gmail.com>
2021-05-25 23:19:49 +0900

    Articleモデルに多対多のリレーションを追加

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tag;
 
 class Article extends Model
 {
@@ -31,4 +32,14 @@ class Article extends Model
         'created_at' => 'datetime:Y-m-d',
         'updated_at' => 'datetime:Y-m-d'
     ];
+
+    /**
+     * The tags that belong to the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'article_tags', 'article_id', 'tag_id');
+    }
 }
